dfe9a88ced8f03a7f01e61a0693509c36d61883d
Carlos Martinez <curlosmart@gmail.com>
2021-03-11 11:56:47 -0500

    refactored code

--- Day 19/turtle-race/main.py
+++ Day 19/turtle-race/main.py
@@ -1,34 +1,29 @@
 from turtle import Turtle, Screen
 from random import randrange
 
-purple = Turtle()
-blue = Turtle()
-green = Turtle()
-yellow = Turtle()
-orange = Turtle()
-red = Turtle()
-
 screen = Screen()
 
-turtles = [purple, blue, green, yellow, orange, red]
-turtle_names = ["purple", "blue", "green", "yellow", "orange", "red"]
+turtles = []
+colors = ["purple", "blue", "green", "yellow",
+          "orange", "red", "black"]
 
 
 def get_in_start_pos():
-    for i, turtle in enumerate(turtles):
-        turtle.color(turtle_names[i])
-        turtle.shape("turtle")
-        turtle.penup()
+    for i, color in enumerate(colors):
+        newTurtle = Turtle()
+        newTurtle.color(color)
+        newTurtle.shape("turtle")
+        newTurtle.penup()
+        turtles.append(newTurtle)
 
         # First turtle (the purple one) will be set to the top left position
         if i == 0:
-            turtle.goto((screen.window_width() // 2) * -1 + 20,
-                        screen.window_height() // 4)
+            newTurtle.goto((screen.window_width() // 2) * -1 + 20,
+                           screen.window_height() // 4)
         # Each following turtle will be 80 pixels below the previous turtle
         else:
-            turtle.goto(turtles[i - 1].pos()[0], turtles[i - 1].pos()[1] - 80)
-
-        turtle.pendown()
+            newTurtle.goto(turtles[i - 1].pos()[0],
+                           turtles[i - 1].pos()[1] - 80)
 
 
 def start_race():
@@ -36,17 +31,17 @@ def start_race():
 
     while finished != True:
         for turtle in turtles:
-            # If the turtle reaches the end of the x-axis (positive)
+            # If the turtle reaches the end of the x-axis (positive), then it reached the finish line so end the program
             if turtle.pos()[0] >= screen.window_width() // 2:
                 finished = True
                 winner = turtle.color()[0]
 
                 if bet == winner:
                     print(
-                        f"You won. The {turtle.color()[0]} turtle has won the race!")
+                        f"You won. The {winner} turtle has won the race!")
                 else:
                     print(
-                        f"You lose. The {turtle.color()[0]} turtle has won the race!")
+                        f"You lose. The {winner} turtle has won the race!")
                 screen.bye()
                 break
             else:
@@ -54,11 +49,6 @@ def start_race():
                 turtle.forward(randrange(50))
 
 
-def get_mouse_click_coor(x, y):
-    print(x, y)
-
-
-screen.onscreenclick(get_mouse_click_coor)
 bet = screen.textinput(
     "Make your bet", "Who will win the race? Enter a colour:")
 get_in_start_pos()
