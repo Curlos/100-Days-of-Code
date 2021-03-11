15e69fa1c9ddc7b000c901971b7b7632b9c9c77a
Carlos Martinez <curlosmart@gmail.com>
2021-03-11 11:15:12 -0500

    Day 19 - Turtle Race (complete)

---
+++ Day 19/turtle-race/main.py
@@ -0,0 +1,60 @@
+from turtle import Turtle, Screen
+from random import randrange
+
+purple = Turtle()
+blue = Turtle()
+green = Turtle()
+yellow = Turtle()
+orange = Turtle()
+red = Turtle()
+
+screen = Screen()
+
+turtles = [purple, blue, green, yellow, orange, red]
+turtle_names = ["purple", "blue", "green", "yellow", "orange", "red"]
+
+
+def get_in_start_pos():
+    for i, turtle in enumerate(turtles):
+        turtle.color(turtle_names[i])
+        turtle.shape("turtle")
+        turtle.penup()
+        if i == 0:
+            turtle.goto((screen.window_width() // 2) * -1 + 20,
+                        screen.window_height() // 4)
+        else:
+            turtle.goto(turtles[i - 1].pos()[0], turtles[i - 1].pos()[1] - 80)
+
+        turtle.pendown()
+
+
+def start_race():
+    finished = False
+
+    while finished != True:
+        for turtle in turtles:
+            if turtle.pos()[0] >= screen.window_width() // 2:
+                finished = True
+                winner = turtle.color()[0]
+                if bet == winner:
+                    print(
+                        f"You won. The {turtle.color()[0]} turtle has won the race!")
+                else:
+                    print(
+                        f"You lose. The {turtle.color()[0]} turtle has won the race!")
+                screen.bye()
+                break
+            else:
+                turtle.penup()
+                turtle.forward(randrange(50))
+
+
+def get_mouse_click_coor(x, y):
+    print(x, y)
+
+
+screen.onscreenclick(get_mouse_click_coor)
+bet = screen.textinput(
+    "Make your bet", "Who will win the race? Enter a colour:")
+get_in_start_pos()
+start_race()
