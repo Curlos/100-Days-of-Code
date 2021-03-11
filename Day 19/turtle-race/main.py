2bcf5d4e2122114fe5cc47decff0a9e0b4389d02
Carlos Martinez <curlosmart@gmail.com>
2021-03-11 11:19:38 -0500

    Added additional comments (turtle-race)

--- Day 19/turtle-race/main.py
+++ Day 19/turtle-race/main.py
@@ -19,9 +19,12 @@ def get_in_start_pos():
         turtle.color(turtle_names[i])
         turtle.shape("turtle")
         turtle.penup()
+
+        # First turtle (the purple one) will be set to the top left position
         if i == 0:
             turtle.goto((screen.window_width() // 2) * -1 + 20,
                         screen.window_height() // 4)
+        # Each following turtle will be 80 pixels below the previous turtle
         else:
             turtle.goto(turtles[i - 1].pos()[0], turtles[i - 1].pos()[1] - 80)
 
@@ -33,9 +36,11 @@ def start_race():
 
     while finished != True:
         for turtle in turtles:
+            # If the turtle reaches the end of the x-axis (positive)
             if turtle.pos()[0] >= screen.window_width() // 2:
                 finished = True
                 winner = turtle.color()[0]
+
                 if bet == winner:
                     print(
                         f"You won. The {turtle.color()[0]} turtle has won the race!")
