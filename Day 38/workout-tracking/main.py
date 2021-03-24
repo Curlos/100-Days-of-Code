83e90250a9077ed5d7b00aff3ede5c2e87b05d5f
Carlos Martinez <curlosmart@gmail.com>
2021-03-24 16:53:33 -0400

    Day 38 - Saving Data into Google Sheets

--- Day 38/workout-tracking/main.py
+++ Day 38/workout-tracking/main.py
@@ -1,22 +1,51 @@
 from dotenv import load_dotenv
+from datetime import datetime
 import requests
+import json
 import os
 
 load_dotenv()
 
 APP_ID = os.getenv("APP_ID")
 API_KEY = os.getenv("API_KEY")
+USER_ID = os.getenv("USER_ID")
 
 headers = {
     "x-app-id": APP_ID,
     "x-app-key": API_KEY,
-    "x-remote-user-id": "0"
+    "x-remote-user-id": "0",
 }
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+add_workout_endpoint = f"https://api.sheety.co/{USER_ID}/myWorkouts/workouts"
 query = {
     "query": "I ran 3 miles and walked 2 miles"
 }
 
 response = requests.post(url=exercise_endpoint, data=query, headers=headers)
-print(response.json())
+exercises = response.json()["exercises"]
+print(exercises)
+
+post_headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+    "x-remote-user-id": "0",
+    "Content-Type": "application/json"
+}
+
+for exercise in exercises:
+    workout = {
+        "workout": {
+            "date": datetime.now().strftime('%x'),
+            "time": datetime.now().strftime('%X'),
+            "exercise": exercise["name"].title(),
+            "duration": round(exercise["duration_min"]),
+            "calories": round(exercise["nf_calories"])
+        }
+    }
+
+    workout_json = json.dumps(workout)
+
+    workout_response = requests.post(url=add_workout_endpoint, data=workout_json, headers=post_headers)
+    print(workout_response)
+    print(workout_response.json())
