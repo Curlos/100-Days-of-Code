b741e3160c8820d72214e82976c8ff6f7b5b8c28
Carlos Martinez <curlosmart@gmail.com>
2021-03-24 16:23:20 -0400

    Day 38 - Get Exercise Stats with Natural Language

--- Day 38/workout-tracking/main.py
+++ Day 38/workout-tracking/main.py
@@ -1,7 +1,22 @@
 from dotenv import load_dotenv
+import requests
 import os
 
 load_dotenv()
 
 APP_ID = os.getenv("APP_ID")
 API_KEY = os.getenv("API_KEY")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+    "x-remote-user-id": "0"
+}
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+query = {
+    "query": "I ran 3 miles and walked 2 miles"
+}
+
+response = requests.post(url=exercise_endpoint, data=query, headers=headers)
+print(response.json())
