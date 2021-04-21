e9cb0c6a8582325ce190eeb408aa41e1fcb1b976
Carlos Martinez <curlosmart@gmail.com>
2021-04-21 16:49:49 -0400

    Day 52 (minor edit)

--- Day 52/main.py
+++ Day 52/main.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 from dotenv import load_dotenv
 import time
 import os
